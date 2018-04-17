d7053490372424432dd39e6fac9946c67e4f3029
Developer <developer@Faz-MacBook-Air.local>
2018-04-17 10:57:45 +0800

    test view controller

--- Ping_MapModule/Controller/ViewController.swift
+++ Ping_MapModule/Controller/ViewController.swift
@@ -2,24 +2,34 @@
 //  ViewController.swift
 //  Ping_MapModule
 //
-//  Created by MacLover on 4/11/18.
+//  Created by Developer on 17/04/2018.
 //  Copyright © 2018 XenoSoft. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UINavigationController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
